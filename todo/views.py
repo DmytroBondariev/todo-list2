3ef6c82ae4456fd5d46b3c6bfd7f6ec5e65f7ea2
Dmytry Bondariev <bondarievmd@gmail.com>
2023-06-25 17:19:56 +0300

    Finished project

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.forms import TagForm, TaskForm
+from todo.models import Task, Tag
+
+
+class IndexView(generic.ListView):
+    model = Task
+    template_name = 'pages/index.html'
+    context_object_name = "task_list"
+    paginate_by = 5
+
+    def post(self, request):
+        pk = request.POST.get('pk')
+
+        task = Task.objects.get(id=pk)
+        if task.is_completed:
+            task.is_completed = False
+        else:
+            task.is_completed = True
+        task.save()
+
+        return HttpResponseRedirect(self.request.path)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "pages/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "pages/tag_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "pages/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "pages/tag_confirm_delete.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "pages/task_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:index")
+    template_name = "pages/task_form.html"
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+    template_name = "pages/task_confirm_delete.html"
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_extensions",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "todo"
 ]
 
 MIDDLEWARE = [
@@ -67,6 +71,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'todo_list.wsgi.application'
 
@@ -117,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
